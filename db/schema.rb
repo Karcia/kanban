3f9f0b72d8a7020d81771e925d02a48c50d72f06
karolina <karciawow@gmail.com>
2017-05-19 12:20:16 +0200

    generate scaffold for lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518084859) do
+ActiveRecord::Schema.define(version: 20170519101640) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
