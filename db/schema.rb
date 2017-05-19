7c2bb5edd242635afcf471dcb52383ae975fa107
karolina <karciawow@gmail.com>
2017-05-19 12:22:51 +0200

    generate scaffold for cards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519101640) do
+ActiveRecord::Schema.define(version: 20170519102202) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "list_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "cards", ["list_id"], name: "index_cards_on_list_id"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
